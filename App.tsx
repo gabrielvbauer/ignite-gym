200c487f47f374b839aba724ab3a9d6e58dbd6c9
gabrielvbauer <gabriel.bauer9@hotmail.com>
2023-09-06 16:52:08 -0300

    feat: SignUp screen

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import { THEME } from './src/theme'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { Loading } from '@components/Loading';
 import { SignIn } from '@screens/SignIn';
+import { SignUp } from '@screens/SignUp';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -21,7 +22,7 @@ export default function App() {
         translucent
       />
       
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <SignUp /> : <Loading />}
     </NativeBaseProvider>
   );
 }
