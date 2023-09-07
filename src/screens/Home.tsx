0add218a395888623a459241daa990ad14da6c86
gabrielvbauer <gabriel.bauer9@hotmail.com>
2023-09-07 13:58:13 -0300

    feat: home screen

---
+++ src/screens/Home.tsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import { HStack, VStack, FlatList, Heading, Text } from "native-base";
+
+import { HomeHeader } from "@components/HomeHeader";
+import { Group } from "@components/Group";
+import { ExerciseCard } from "@components/ExerciseCard";
+
+export function Home() {
+  const [exercises, setExercises] = useState(['Puxada frontal', "Remada curvada", "Remada unilateral", "Levantamento terra"])
+  const [groups, setGroups] = useState(['Costas', "Biceps", "Triceps", "Ombro"])
+  const [selectedGroup, setSelectedGroup] = useState('Costas')
+
+  return (
+    <VStack flex={1}>
+      <HomeHeader />
+
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={String(selectedGroup).toUpperCase() === String(item).toUpperCase()}
+            onPress={() => setSelectedGroup(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{
+          px: 8,
+        }}
+        my={10}
+        maxH={10}
+      />
+
+      <VStack flex={1} px={8}>
+        <HStack
+          justifyContent="space-between"
+          mb={5}
+        >
+          <Heading color="gray.200" fontSize="md">
+            Exercícios
+          </Heading>
+
+          <Text color="gray.200" fontSize="sm">
+            {exercises.length}
+          </Text>
+        </HStack>
+
+        <FlatList 
+          data={exercises}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <ExerciseCard 
+              name={item}
+            />
+          )}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{
+            paddingBottom: 20
+          }}
+        />
+      </VStack>
+    </VStack>
+  )
+}
